205fe20e59b967a283abe8475a38a26e450d5970
hzhangal <hzhangal0330@gmail.com>
2018-04-09 10:57:55 +0800

    test

--- image_captioning/class_soft_att_rcst_LSTM.py
+++ image_captioning/class_soft_att_rcst_LSTM.py
@@ -22,7 +22,7 @@ class ShowAttendTellModel(nn.Module):
         self.vocab_size = opt.vocab_size
         self.input_encoding_size = opt.input_encoding_size
         self.lstm_size = opt.lstm_size
-        self.drop_prob_lm = opt.drop_prob_lm
+        # self.drop_prob_lm = opt.drop_prob_lm
         self.drop_prob_lm = 0.5
         self.seq_length = opt.seq_length
         
